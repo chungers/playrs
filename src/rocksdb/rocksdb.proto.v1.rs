dac410b67c723754d5f439007ca9d834010e9859
David Chung <davidc616+m2air@gmail.com>
2023-12-26 22:55:36 -0800

    Renamed proto and changed proto definitions to store graph

--- src/rocksdb/rocksdb.proto.v1.rs
+++ src/rocksdb/rocksdb.proto.v1.rs
@@ -1,24 +1,24 @@
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DataRecord {
+pub struct Node {
     #[prost(uint64, tag = "1")]
     pub id: u64,
     #[prost(string, tag = "2")]
     pub name: ::prost::alloc::string::String,
     #[prost(string, optional, tag = "3")]
-    pub description: ::core::option::Option<::prost::alloc::string::String>,
+    pub doc: ::core::option::Option<::prost::alloc::string::String>,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Relation {
+pub struct Edge {
     #[prost(uint64, tag = "1")]
     pub id: u64,
     #[prost(string, tag = "2")]
-    pub relation: ::prost::alloc::string::String,
+    pub name: ::prost::alloc::string::String,
     #[prost(uint64, tag = "3")]
     pub head: u64,
     #[prost(uint64, tag = "4")]
     pub tail: u64,
     #[prost(string, optional, tag = "5")]
-    pub description: ::core::option::Option<::prost::alloc::string::String>,
+    pub doc: ::core::option::Option<::prost::alloc::string::String>,
 }
