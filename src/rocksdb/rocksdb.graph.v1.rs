aa28c487d6086b9f8494b6d01465871000aed1c1
David Chung <davidc616@gmail.com>
2024-01-26 23:47:43 -0800

    Fix bad merge

--- src/rocksdb/rocksdb.graph.v1.rs
+++ src/rocksdb/rocksdb.graph.v1.rs
@@ -6,25 +6,19 @@ pub struct Node {
     #[prost(string, tag = "2")]
     pub name: ::prost::alloc::string::String,
     #[prost(string, optional, tag = "3")]
-    pub doc: ::core::option::Option<::prost::alloc::string::String>,
+    pub description: ::core::option::Option<::prost::alloc::string::String>,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Edge {
     #[prost(uint64, tag = "1")]
     pub id: u64,
-<<<<<<<< HEAD:src/rocksdb/graph.proto.v1.rs
-    #[prost(string, tag = "2")]
-    pub name: ::prost::alloc::string::String,
-    #[prost(uint64, tag = "3")]
-========
     #[prost(uint64, tag = "2")]
->>>>>>>> 67429fd (Refactoring open_db; start building a graph db):src/rocksdb/rocksdb.graph.v1.rs
     pub head: u64,
     #[prost(uint64, tag = "3")]
     pub tail: u64,
     #[prost(string, tag = "4")]
     pub name: ::prost::alloc::string::String,
     #[prost(string, optional, tag = "5")]
-    pub doc: ::core::option::Option<::prost::alloc::string::String>,
+    pub description: ::core::option::Option<::prost::alloc::string::String>,
 }
