8dda4ba1bb71fbcad1577e60f769f41dfe584232
David Chung <davidc616+mba-m4@gmail.com>
2025-07-24 10:35:42 -0700

    fix broken build by updating pkgs
    Signed-off-by: David Chung <davidc616+mba-m4@gmail.com>

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn gen_proto(proto_file: &str, out_dir: &str) {
     tonic_build::configure()
         .build_server(true)
         .out_dir(out_dir)
-        .compile(&[proto_file], &["."])
+        .compile_protos(&[proto_file], &["."])
         .unwrap_or_else(|e| panic!("!!! protobuf compile error: {}", e));
 
     println!("cargo:rerun-if-changed={}", proto_file);
